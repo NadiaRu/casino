ba8cd4cffd1b8ffdd569c72d88c017abd4a32425
rumiantn <rumiantceva.nadezhda@etu.univ-grenoble-alpes.fr>
2021-03-30 15:42:54 +0200

    V1 : affichage plus propre pour Linux/Windows ; délimitation de la plage du numéro choisi

--- casino.py
+++ casino.py
@@ -1,14 +1,22 @@
 #-*- encoding utf8 -*-
 
 import random
+import os
+
+#for Windows
+# ~ clear = lambda: os.system('cls')
+#for Linux
+clear = lambda: os.system('clear')
 
 cagnotte = 500
 gains = 0
 continuer = "o"
 while cagnotte > 0 and continuer == "o":
 	numeroGagnant = random.randint(0,49)
-	# ~ print (numeroGagnant)
+	print (numeroGagnant)
 	numeroChoisi = int(input("Choisissez un numéro : "))
+	while numeroChoisi < 0 or numeroChoisi > 49 :
+		numeroChoisi = int(input("Veuillez choisir un numéro entre 0 et 49 : "))
 	sommeMise = int(input("Quelles sera la somme misée ? "))
 	while sommeMise > cagnotte :
 		print ("Vous ne pouvez pas mettre plus d'argent que ce que vous possedez")
@@ -21,6 +29,7 @@ while cagnotte > 0 and continuer == "o":
 		cagnotte = cagnotte+sommeMise+gains
 		print ("Vous avez gagné",str(gains),"euros.\nVotre cagnotte est à",str(cagnotte),"euros actuellement.")
 		continuer = input("Voulez-vous continuer ? [o/n] ")
+		clear()
 	#le cas de la même couleur
 	elif ((numeroChoisi % 2 == 0) and (numeroGagnant % 2 == 0)) or ((numeroChoisi % 2 == 1) and (numeroGagnant % 2 == 1)):
 		print ("Le numéro misé est de la même couleur que le numéro gagnant !")
@@ -28,14 +37,17 @@ while cagnotte > 0 and continuer == "o":
 		cagnotte = cagnotte+sommeMise+gains
 		print ("Vous avez gagné",str(gains),"euros.\nVotre cagnotte est à",str(cagnotte),"euros actuellement.")
 		continuer = input("Voulez-vous continuer ? [o/n] ")
-		#perte
+		clear()
+	#perte
 	else :
 		print ("Vous avez perdu...")
 		print ("Vous avez perdu",str(sommeMise),"euros.\nVotre cagnotte est à",str(cagnotte),"euros actuellement.")
 		if cagnotte == 0 :
+			clear()
 			break
 		continuer = input("Voulez-vous continuer ? [o/n] ")
+		clear()
 if cagnotte == 0 :
 	print ("Vous n'avez plus d'argent... Revenez le mois prochain après le salaire !")
 if continuer == "n" :
-	print ("Vpus partez avec",str(cagnotte),"euros.\nMerci pour votre visite ! À bientôt !")
+	print ("Vous partez avec",str(cagnotte),"euros.\nMerci pour votre visite ! À bientôt !")
